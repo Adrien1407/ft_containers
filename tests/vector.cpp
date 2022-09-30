b8e91edb459cdb4b7b1e8d239620b840968924f4
Adrien LANCELLE <adlancel@e1r9p14.clusters.42paris.fr>
2022-09-30 14:59:29 +0200

    fuck vector

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -38,6 +38,24 @@ public:
 private:
     int _n;
 };
+
+template <class T>
+void print_vector(NAMESPACE::vector<T> a)
+{
+    if (a.capacity() != 0)
+    {
+        // std::cout << print++ << _NC << "  > ";
+        std::cout << "vector contains : ";
+        for (typename NAMESPACE::vector<T>::iterator it = a.begin(); it != a.end(); it++)
+            std::cout << *it << ", ";
+        // std::cout << "   Size of vector -> " << a.size() << std::endl;
+        std::cout << std::endl;
+    }
+    else
+    {
+        std::cerr << "this vector is empty ... " << std::endl;
+    }
+}
 std::ostream &operator<<(std::ostream &o, Awesome const &rhs)
 {
     o << rhs.get();
@@ -93,126 +111,156 @@ void vector_comparision_operator()
 
     NAMESPACE::vector<int> vec_int(4, 8);
     NAMESPACE::vector<double> vec_double(4, 8.00);
-    NAMESPACE::vector<std::string> vec_string(4, "bonjour");
-    NAMESPACE::vector<NAMESPACE::vector<std::string> > vec_vec_string(4, vec_string);
+    // NAMESPACE::vector<std::string> vec_string(4, "bonjour");
+    // NAMESPACE::vector<NAMESPACE::vector<std::string> > vec_vec_string(4, vec_string);
 
     NAMESPACE::vector<int>::iterator iti = vec_int.begin();
     NAMESPACE::vector<double>::iterator itd = vec_double.begin();
-    NAMESPACE::vector<std::string>::iterator its = vec_string.begin();
+    // NAMESPACE::vector<std::string>::iterator its = vec_string.begin();
     // NAMESPACE::vector<NAMESPACE::vector<std::string> >::iterator itv = vec_vec_string.begin();
 
     std::cout << *iti << " == " << *itd << " ?";
     (*iti == *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " != " << *itd << " ?";
     (*iti != *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " >= " << *itd << " ?";
     (*iti >= *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " <= " << *itd << " ?";
     (*iti <= *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " > " << *itd << " ?";
     (*iti > *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " < " << *itd << " ?";
     (*iti < *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << std::endl;
     std::cout << "we modify left part" << std::endl;
     std::cout << std::endl;
 
     *iti = 2;
 
-      std::cout << *iti << " == " << *itd << " ?";
+    std::cout << *iti << " == " << *itd << " ?";
     (*iti == *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " != " << *itd << " ?";
     (*iti != *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " >= " << *itd << " ?";
     (*iti >= *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " <= " << *itd << " ?";
     (*iti <= *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " > " << *itd << " ?";
     (*iti > *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
-    
+
     std::cout << *iti << " < " << *itd << " ?";
     (*iti < *itd) ? std::cout << " yes" : std::cout << " no";
     std::cout << std::endl;
 
+    // std::cout << "string comparision" << std::endl;
 
-std::cout << "string comparision" << std::endl;
+    // std::cout << *its << " == " << *itv << " ?";
+    // (*its == *itv) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
 
-std::cout << *its << " == " << *itv << " ?";
-    (*its == *itv) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *its << " != " << *itv << " ?";
-    (*its != *itv) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *its << " >= " << *itv << " ?";
-    (*its >= *itv) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *its << " <= " << *itv << " ?";
-    (*its <= *itv) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *its << " > " << *itv << " ?";
-    (*its > *itv) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *its << " < " << *itv << " ?";
-    (*its < *itv) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << std::endl;
-    std::cout << "we modify left part" << std::endl;
-    std::cout << std::endl;
+    // std::cout << *its << " != " << *itv << " ?";
+    // (*its != *itv) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
 
-    *its = "hello";
+    // std::cout << *its << " >= " << *itv << " ?";
+    // (*its >= *itv) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
 
-      std::cout << *its << " == " << *(its + 1) << " ?";
-    (*its == *(its + 1)) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *its << " != " << *(its + 1) << " ?";
-    (*its != *(its + 1)) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *iti << " >= " << *(its + 1) << " ?";
-    (*iti >= *(its + 1)) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *iti << " <= " << *(its + 1) << " ?";
-    (*iti <= *(its + 1)) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *iti << " > " << *(its + 1) << " ?";
-    (*iti > *(its + 1)) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
-    
-    std::cout << *iti << " < " << *(its + 1) << " ?";
-    (*iti < *(its + 1)) ? std::cout << " yes" : std::cout << " no";
-    std::cout << std::endl;
+    // std::cout << *its << " <= " << *itv << " ?";
+    // (*its <= *itv) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
 
+    // std::cout << *its << " > " << *itv << " ?";
+    // (*its > *itv) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
 
+    // std::cout << *its << " < " << *itv << " ?";
+    // (*its < *itv) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
+
+    // std::cout << std::endl;
+    // std::cout << "we modify left part" << std::endl;
+    // std::cout << std::endl;
+
+    // *its = "hello";
+
+    // std::cout << *its << " == " << *(its + 1) << " ?";
+    // (*its == *(its + 1)) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
+
+    // std::cout << *its << " != " << *(its + 1) << " ?";
+    // (*its != *(its + 1)) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
+
+    // std::cout << *iti << " >= " << *(its + 1) << " ?";
+    // (*iti >= *(its + 1)) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
+
+    // std::cout << *iti << " <= " << *(its + 1) << " ?";
+    // (*iti <= *(its + 1)) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
+
+    // std::cout << *iti << " > " << *(its + 1) << " ?";
+    // (*iti > *(its + 1)) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
+
+    // std::cout << *iti << " < " << *(its + 1) << " ?";
+    // (*iti < *(its + 1)) ? std::cout << " yes" : std::cout << " no";
+    // std::cout << std::endl;
+}
+
+template <class T>
+void vector_capacity_test(NAMESPACE::vector<T> myvec)
+{
+    std::cout << std::endl;
+    std::cout << "CAPACITY/SIZE" << std::endl;
+    std::cout << std::endl;
+    print_vector(myvec);
+
+    std::cout << "myvec_capacity = " << myvec.capacity() << std::endl;
+    std::cout << "myvec_size = " << myvec.size() << std::endl;
+    std::cout << "myvec is empty ? ";
+    (myvec.empty()) ? std::cout << "yes" : std::cout << "no";
+    std::cout << std::endl;
+    std::cout << std::endl;
+    while (std::cin.get() != '\n')
+    {
+    }
+    std::cout << "we delete 1 value" << std::endl;
+    myvec.erase(myvec.begin());
+
+    std::cout << "myvec_capacity = " << myvec.capacity() << std::endl;
+    std::cout << "myvec_size = " << myvec.size() << std::endl;
+    print_vector(myvec);
+    while (std::cin.get() != '\n')
+    {
+    }
+    for (int i = 0; i < 30; i++)
+        myvec.push_back(*myvec.begin());
+    print_vector(myvec);
+    std::cout << "myvec_capacity = " << myvec.capacity() << std::endl;
+    std::cout << "myvec_size = " << myvec.size() << std::endl;
 }
 
 // std::cout << "!=" std::endl;
@@ -223,7 +271,7 @@ std::cout << *its << " == " << *itv << " ?";
 
 void vector_test()
 {
-    std::cout << "==============================   " << USING << "    =======================================" << std::endl;
+    std::cout << "===============================   " << USING << "   =======================================" << std::endl;
 
     std::cout << std::endl;
     std::cout << std::setw(40) << "ITERATORS" << std::endl;
@@ -231,11 +279,16 @@ void vector_test()
     // int
     {
         std::cout << std::setw(20) << "INT VECTOR" << std::endl;
-        NAMESPACE::vector<int> myvec(3, 3);
+        NAMESPACE::vector<int> myvec;
+        for (int i = 0; i < 10; i++)
+        {
+            myvec.push_back(i * 4);
+        }
         vector_begin_test(myvec);
         vector_end_test(myvec);
         vector_rbegin_test(myvec);
         vector_rend_test(myvec);
+        vector_capacity_test(myvec);
         while (std::cin.get() != '\n')
         {
         }
@@ -243,36 +296,59 @@ void vector_test()
 
     // double
     {
-        NAMESPACE::vector<double> myvec(3, 3.14);
+        NAMESPACE::vector<double> myvec;
+        for (int i = 0; i < 10; i++)
+        {
+            myvec.push_back(i + 0.002);
+        }
         std::cout << std::setw(20) << "DOUBLE VECTOR" << std::endl;
         vector_begin_test(myvec);
         vector_end_test(myvec);
         vector_rbegin_test(myvec);
         vector_rend_test(myvec);
+        vector_capacity_test(myvec);
         while (std::cin.get() != '\n')
         {
         }
     }
     // char
     {
-        NAMESPACE::vector<char> myvec(3, 'a');
+        NAMESPACE::vector<char> myvec;
+        for (int i = 0; i < 10; i++)
+        {
+            myvec.push_back('a' + 2 * i);
+        }
+
         std::cout << std::setw(20) << "CHAR VECTOR" << std::endl;
         vector_begin_test(myvec);
         vector_end_test(myvec);
         vector_rbegin_test(myvec);
         vector_rend_test(myvec);
+        vector_capacity_test(myvec);
         while (std::cin.get() != '\n')
         {
         }
     }
     // str
     {
-        NAMESPACE::vector<std::string> myvec(3, "bonjour");
+        std::string tab[4] = {
+            "bonjour",
+            "le",
+            "correcteur",
+            "!"};
+
+        NAMESPACE::vector<std::string> myvec;
+        for (int i = 0; i < 4; i++)
+        {
+            myvec.push_back(tab[i]);
+        }
+
         std::cout << std::setw(20) << "STRING VECTOR" << std::endl;
         vector_begin_test(myvec);
         vector_end_test(myvec);
         vector_rbegin_test(myvec);
         vector_rend_test(myvec);
+        vector_capacity_test(myvec);
         while (std::cin.get() != '\n')
         {
         }
@@ -290,6 +366,7 @@ void vector_test()
         vector_end_test(myvec);
         vector_rbegin_test(myvec);
         vector_rend_test(myvec);
+        vector_capacity_test(myvec);
         while (std::cin.get() != '\n')
         {
         }
